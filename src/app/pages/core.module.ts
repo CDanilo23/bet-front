95062a1233682fa089ecf7edbc1f15691b0af85e
Cristian <cristiand.ordonez@sophossolutions.com>
2020-08-03 10:34:17 -0500

    fixes migrate to angular 10

--- src/app/pages/core.module.ts
+++ src/app/pages/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatRadioModule, MatButtonModule, MatNativeDateModule, MatTableModule, MatCardModule, MatInputModule, MatCheckboxModule } from '@angular/material';
-import { MatMenuModule, MatIconModule } from '@angular/material';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableModule} from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule} from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+
 
 @NgModule({
   imports: [
